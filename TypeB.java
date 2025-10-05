5d10213c22fe8c8892abca9d8a3b33c30b756974
Adam Butt <adambutt@Adams-MacBook-Pro.local>
2025-10-05 00:25:20 -0400

    I finished the TypeB file

---
+++ TypeB.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class TypeB extends Thing {
+    int timeSinceLast;
+
+    public TypeB (int row, int col, int dir, char lab) {
+        super(row, col, dir, lab);
+        this.timeSinceLast = 0;
+    }
+
+    @Override
+    public void maybeTurn(Random rand) {
+        int i = rand.nextInt(3);
+        this.timeSinceLast++;
+
+        if (this.timeSinceLast == 10) {
+            this.timeSinceLast = 0;
+
+            if (i == 1) {
+                rightTurn();
+            }
+
+            if (i == 2) {
+                leftTurn();
+            }
+        }
+    }
+}
